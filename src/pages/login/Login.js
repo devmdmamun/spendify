d707155654e3c0902247b85f58a8bb271f0ea2ca
Mamun <md.mamun.islam.santo@gmail.com>
2022-01-09 19:37:02 +0600

    added firebase cli, edit firestore rules, hosted the app in firebase

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./Login.module.css";
 import { useLogin } from "../../hooks/useLogin";
 
@@ -13,31 +14,36 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles["login-form"]}>
-      <h2>Log in</h2>
-      <label>
-        <span>Email:</span>
-        <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>Password:</span>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      {!isPending && <button className="btn">Login</button>}
-      {isPending && (
-        <button className="btn" disabled>
-          Loading
-        </button>
-      )}
-      {error && <p>{error}</p>}
-    </form>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles["login-form"]}>
+        <h2>Log in</h2>
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        {!isPending && <button className="btn">Login</button>}
+        {isPending && (
+          <button className="btn" disabled>
+            Loading
+          </button>
+        )}
+        {error && <p>{error}</p>}
+      </form>
+      <p className={styles.create}>
+        Don't have an account? <Link to="/signup">Create here.</Link>{" "}
+      </p>
+    </div>
   );
 }
